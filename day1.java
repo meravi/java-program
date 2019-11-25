cb9bc63c271f5edb06c9d7e99067adbf3abcc436
Developer Ravi <35109316+meravi@users.noreply.github.com>
2019-11-25 17:04:00 +0545

    Add files via upload
    WAP to print msg inline and block

---
+++ day1.java
@@ -0,0 +1,12 @@
+// WAP to print msg inline and block 
+class day1{
+	public static void main(String args[]){
+// for block display
+		System.out.println("Hello");
+		System.out.println("World");
+
+// for inline display	
+   System.out.print("Hello");	
+   System.out.print("World");
+}
+}
