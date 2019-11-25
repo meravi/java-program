46a5cab9085fd63edee46f24185047114b516719
Developer Ravi <35109316+meravi@users.noreply.github.com>
2019-11-25 17:01:46 +0545

    Add files via upload
    Simple Hello World Print ... Block and Inline

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+class HelloWorld{
+	public static void main(String args[]){
+// for block display
+		System.out.println("Hello");
+		System.out.println("World");
+
+// for inline display	
+   System.out.print("Hello");	
+   System.out.print("World");
+}
+}
